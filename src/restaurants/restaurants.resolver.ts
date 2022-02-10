ce0d5fac485d1768e4bbbb1786923594f7bcf526
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-10 12:25:35 +0900

    build: ObjectType, Arguments, ArgsType, Validation => graphQL API DONE

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restqurant.entity';
 
-@Resolver()
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Boolean)
-  isPizzaGood(): Boolean {
+  @Query((returns) => [Restaurant]) // GraphQL 방식
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
     return true;
   }
 }
