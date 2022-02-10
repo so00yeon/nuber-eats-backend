02d383bab851040bd05c392417c49527d021c0d6
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-10 15:30:33 +0900

    build: Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // 애플리케이션의 어디서나 config 모듈에 접근할 수 있음
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test', // 파일경로
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 deploy할 때 환경변수 파일을 사용하지 않음
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'datamond',
-      password: '12345',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
