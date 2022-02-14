e423ce270391a19a30b7b8b0a85badde3fb22c40
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-14 17:42:29 +0900

    chore: edit middleware - use class

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,6 +1,9 @@
+import { NestMiddleware } from '@nestjs/common';
 import { NextFunction } from 'express';
 
-export function jwtMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(req.headers);
-  next();
+export class JwtMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(req.headers);
+    next();
+  }
 }
