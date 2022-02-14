a39c61953d1a13a6b10367be30acae3abff7e744
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-14 23:58:19 +0900

    feat: authorization - use guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -46,7 +47,6 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
       context: ({ req }) => ({ user: req['user'] }),
     }),
     UsersModule,
-    CommonModule,
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
