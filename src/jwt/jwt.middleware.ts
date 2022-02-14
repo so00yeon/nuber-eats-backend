596f8e4a5fe7eeb34444b46b0066b963770d9bd9
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-14 18:25:28 +0900

    chore: find user middleware

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,9 +1,25 @@
-import { NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from './jwt.service';
 
+@Injectable()
 export class JwtMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.headers);
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {} // @Injectable() 여야만 inject 가능
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt'];
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.userService.findById(decoded['id']);
+          console.log(user);
+        } catch (e) {}
+      }
+    }
     next();
   }
 }
