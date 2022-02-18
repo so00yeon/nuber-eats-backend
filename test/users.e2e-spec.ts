0a5019ae5a366e74ae7dd2f0ca86258aaf7362dc
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-18 12:31:08 +0900

    chore: some edit

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -188,13 +188,43 @@ describe('UserModule (e2e)', () => {
               },
             },
           } = res;
-          console.log(res.body);
           expect(ok).toBe(true);
           expect(error).toBe(null);
           expect(id).toBe(userId);
         });
     });
-    it.todo('should not find a profile');
+
+    it('should not find a profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+          {
+            userProfile(userId:666) {
+              ok
+              error
+              user {
+                id
+              }
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                userProfile: { ok, error, user },
+              },
+            },
+          } = res;
+          expect(ok).toBe(false);
+          expect(error).toBe('User Not Found');
+          expect(user).toBe(null);
+        });
+    });
   });
   it.todo('me');
   it.todo('verifyEmail');
